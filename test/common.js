63ac40f91cc8421fad2d283d084faa7d23950816
Blade Barringer <blade@crookedneighbor.com>
2017-06-10 23:51:36 -0500

    Convert the tests away from CoffeeScript
    Also makes them stop relying on global variables.

--- test/common.js
+++ test/common.js
@@ -1,17 +1,14 @@
 "use strict";
 
-global.chai = require("chai");
-global.should = require("chai").should();
-global.expect = require("chai").expect;
-global.AssertionError = require("chai").AssertionError;
+var chai = require("chai");
+var sinonChai = require("../lib/sinon-chai");
+chai.use(sinonChai);
+chai.should();
 
-global.swallow = function (thrower) {
+exports.swallow = function (thrower) {
     try {
         thrower();
     } catch (e) {
       // Intentionally swallow
     }
 };
-
-var sinonChai = require("../lib/sinon-chai");
-global.chai.use(sinonChai);
