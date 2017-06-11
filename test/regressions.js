166fc9db562b54baee0356ecf5a914256e4190e4
Domenic Denicola <d@domenic.me>
2017-06-11 00:08:23 -0400

    Fix linting and rebase

--- test/regressions.js
+++ test/regressions.js
@@ -1,25 +1,34 @@
 "use strict";
 
 var sinon = require("sinon");
+var expect = require("chai").expect;
 
-describe("Regressions", function() {
-  specify("GH-19: functions with `proxy` properties", function() {
-    var func = function() {};
-    func.proxy = 5;
+describe("Regressions", function () {
+    specify("GH-19: functions with `proxy` properties", function () {
+        function func() {
+            // Contents don't matter
+        }
+        func.proxy = 5;
 
-    var spy = sinon.spy(func);
-    spy();
+        var spy = sinon.spy(func);
+        spy();
 
-    expect(function () { spy.should.have.been.called; }).to.not.throw();
-  });
+        expect(function () {
+            spy.should.have.been.called;
+        }).to.not.throw();
+    });
 
-  specify("GH-94: assertions on calls", function() {
-    var func = function() {};
-    var spy = sinon.spy(func);
+    specify("GH-94: assertions on calls", function () {
+        function func() {
+            // Contents don't matter
+        }
+        var spy = sinon.spy(func);
 
-    spy(1, 2, 3);
-    spy(4, 5, 6);
+        spy(1, 2, 3);
+        spy(4, 5, 6);
 
-    expect(function () { spy.lastCall.should.have.been.calledWith(4, 5, 6); }).to.not.throw();
-  });
+        expect(function () {
+            spy.lastCall.should.have.been.calledWith(4, 5, 6);
+        }).to.not.throw();
+    });
 });
