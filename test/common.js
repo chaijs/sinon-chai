bdb07e171f4cc10c01fc924b687fd0a9614b94a6
Luiz Américo Pereira Câmara <camara_luiz@yahoo.com.br>
2021-06-28 18:41:30 -0300

    Adjust test setup to load ES module source code

--- test/common.js
+++ test/common.js
@@ -1,10 +1,5 @@
 "use strict";
 
-var chai = require("chai");
-var sinonChai = require("../lib/sinon-chai");
-chai.use(sinonChai);
-chai.should();
-
 exports.swallow = function (thrower) {
     try {
         thrower();
---
+++ test/setup.mjs
@@ -0,0 +1,4 @@
+import chai from "chai";
+import sinonChai from "../lib/sinon-chai.mjs";
+chai.use(sinonChai);
+chai.should();
